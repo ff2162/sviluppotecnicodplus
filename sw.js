f679efd77f25eaccb5bd8e06d5bff87c42695c99
ff2162 <ff@dplusitalia.it>
2026-05-13 00:47:26 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'dplus-mach1-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './logo-192.png',
+  './logo-512.png'
+];
+
+// Installazione: scarica i file nella cache
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Funzionamento offline: se non c'è rete, usa i file in cache
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
